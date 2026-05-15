e0599d6048d2c4f56a5cd23900cf381be5d2c5ad
bizhab <smpaisyiyah01@gmail.com>
2026-05-15 18:08:55 +0800

    feat: autentikasi login backend

--- resources/js/Pages/Pengajuan/DaftarPengajuan.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Daftar Pengajuan</h1>
-  </div>
-</template>
-
-<script setup>
-</script>
--- routes/api.php
+++
@@ -1,9 +0,0 @@
-use Illuminate\Support\Facades\Route;
-
-Route::get('/count', function () {
-    return response()->json(['count' => 10]);
-});
-
-Route::post('/count/increment', function () {
-    return response()->json(['count' => 11]);
-});
